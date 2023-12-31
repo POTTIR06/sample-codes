75cffa212271ee795cdcfa3ea88f163f6b6e2098
POTTIR06 <154866855+POTTIR06@users.noreply.github.com>
2023-12-31 15:05:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+ 
+namespace ClassExample
+{
+    // Car class
+    public class Car
+    {
+        // Data members
+        public string brand = "Hyundai";
+        public string model = "Creta";
+        public string color = "Red";
+        public int topSpeed = 165;
+
+        public void CarDetails()
+        {
+            Console.WriteLine($"Brand:{brand} Model:{model} Color: {color}: Speed:{topSpeed}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating object of the Car class
+            Car carObj = new Car();
+            Console.WriteLine("Print Car Details");
+            carObj.CarDetails();
+
+        }
+    }
+}
